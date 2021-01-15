b54deea4cbe6ddde58a7f3a6c4bf85eb3fd6995c
phoebe njoroge <phoebe.njoroge@gmail.com>
2021-01-15 11:40:04 -0600

    first commit

---
+++ js/movie.js
@@ -0,0 +1,31 @@
+$(document).ready(function (){
+const movieAPIURL = 'https://reminiscent-marshy-galliform.glitch.me/movies';
+const getMovie = function(movie){
+
+    // $("#displayMovie").html("<p>" + '<strong>' + 'Movie Name: ' + '</strong>' + movie[0].title + "</p>");
+    for (var i = 0; i < movie.length; i++) {
+
+            var content =
+                '<tr><th>' + 'Name' +'</th>'+
+                '<th>'+ 'Year' + '</th>'+
+                '<th>'+ 'Genre' + '</th>'+
+                '<th>'+ 'Rating' + '</th> </tr>'+
+
+               "<tr> <td>" +  movie[i].title +  "</td> " +
+                "<td>" +  movie[i].year +  "</td>" +
+                "<td>" +  movie[i].genre +  "</td>" +
+                "<td>" +  movie[i].rating +  "</td> </tr>"
+
+            $('#movieList').append(content)
+
+
+        }
+
+    console.log(movie);
+
+}
+fetch(movieAPIURL).then(function (response) {
+   response.json().then(getMovie)
+})
+
+})
